f2991e0aa8f3eb14ffcb779c84419dfe0944b97f
Ryo <rchiba8030@gmail.com>
2023-12-01 20:31:11 +0900

    3

---
+++ src/routes/admin/selections/min/AddNewMidselModal.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { fly, fade } from 'svelte/transition'
+	export let visible = false
+</script>
+
+{#if visible}
+<div class="modal" transition:fly= {{ y: -200, duration: 300 }}>
+	<slot />
+</div>
+<div class="overlay" transition:fade={{ duration: 200 }}></div>
+{/if}
+
+<style>
+	.modal {
+		width: 84vw;
+		min-height: 10vh;
+		padding: 24px;
+		background: #bad3ff;
+		z-index: 5;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		border-radius: 6px;
+		opacity: 1;
+	}
+	.overlay {
+		position: fixed;
+		top :0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background: rgba(0,0,0,0.5);
+		z-index: 2;
+	}
+</style>
---
+++ src/routes/admin/selections/store.js
@@ -0,0 +1,5 @@
+import { writable } from 'svelte/store'
+
+export const midselStore = writable([])
+export const minselStore = writable([])
+export const majaChoicesStore = writable([])
