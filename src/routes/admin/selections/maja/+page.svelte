685485af9519db126a9d7f255ae1266a25b0adb0
Ryo <rchiba8030@gmail.com>
2023-12-01 23:55:55 +0900

    4mm

--- src/routes/admin/selections/maja/+page.svelte
+++ src/routes/admin/selections/maja/+page.svelte
@@ -8,12 +8,47 @@
 	export let data
 	majaChoicesStore.set(JSON.parse(data["majaselections"]))
 
-	console.log("$majaChoicesStore: ", $majaChoicesStore)
+	const handleAddPost = async () => {
 
+	}
+	const handleInsertCsv = async () => {
+
+	}
+	const handleMultiRowDelete = async () => {
+		
+	}
+	const openMajaselEditModal = async (row) => {
+
+	}
+
+	const handleDeleteButton = async (row) => {
+
+	}
 </script>
 
 <section>
-	{#each $majaChoicesStore as row}
-		<li>{row.text}</li>
-	{/each}
+	<h2 style="width: 100vw; display: inline-block; text-align: center; background-color: #fecca2;">大分類選択肢管理</h2>
+
+	<button style="border: 1px solid;" on:click={() => handleAddPost()}>新規登録</button>
+	<button style="border: 1px solid;" on:click={() => handleInsertCsv()}>CSV一括登録</button>
+	<button style="border: 1px solid;" on:click={() => handleMultiRowDelete()}>範囲指定削除</button>
+	<table>
+	<thead>
+		<tr>
+			<th>itemId</th>
+			<th>テキスト</th>
+			<th colspan="2">操作</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each $majaChoicesStore as row}
+			<tr>
+				<td>{row.itemId}</td>
+				<td>{row.text}</td>
+				<td class="btn-action"><button class="btn-edit" on:click={() => openMajaselEditModal(row)}>編集</button></td>
+				<td class="btn-action"><button class="btn-delete" on:click={() => handleDeleteButton(row)}>削除</button></td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
 </section>
