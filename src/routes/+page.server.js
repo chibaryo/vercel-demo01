5e0f589328435d5710e191d9969f6fccadfa2489
Ryo <rchiba8030@gmail.com>
2023-12-02 08:39:58 +0900

    1stm

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,10 +1,10 @@
-import { API_KEY } from '$env/static/private'
+//import { API_KEY } from '$env/static/private'
 
 /** @type {import('./$types').PageServerLoad} */
 export const load = (async ({ params }) => {
 
 	return { 
 		message: 'ok',
-		yourkey: API_KEY
+//		yourkey: API_KEY
 	}
 })
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,7 @@
 </script>
 
 <section>
-	{data.yourkey}
+<!--	{data.yourkey} -->
+	<a href="/chatmain">Chatmain</a>
+	<a href="/admin">管理画面</a>
 </section>
