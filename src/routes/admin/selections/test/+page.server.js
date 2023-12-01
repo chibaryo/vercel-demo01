7c09d541fd965cdaabe61fdefda55d744ee16c0f
Ryo <rchiba8030@gmail.com>
2023-12-01 20:48:39 +0900

    3

--- src/routes/admin/selections/test/+page.server.js
+++ src/routes/admin/selections/test/+page.server.js
@@ -2,15 +2,14 @@ import { GrandChildModel, SelectionModel } from '$lib/mongodb/models/Selection'
 import { connectDB } from '$lib/mongodb/plugins/dbconnection'
 
 export const load = (async () => {
-/*	const minresp = await fetch(`/api/selections/min`, {
+	const minresp = await fetch(`/api/selections/min`, {
 		method: 'GET',
 	})
 	const min_retdata = await minresp.json()
-*/
-	await connectDB()
-	// Get min_selections[]
-	const resp = await GrandChildModel.find({}).populate('parentId').sort({ "parentId.text": 1 }).lean()
-	console.log("resp: ", resp)
+
+//	await connectDB()
+//	const resp = await GrandChildModel.find({}).populate('parentId').sort({ "parentId.text": 1 }).lean()
+	console.log("min_retdata: ", min_retdata)
 
   return {
 //		minselections: min_retdata["minselections"]
