ce69be1044f939ed56a80862842544d9ae69ec53
Ryo <rchiba8030@gmail.com>
2023-12-02 00:11:46 +0900

    4mm

--- src/routes/admin/selections/maja/+page.server.js
+++ src/routes/admin/selections/maja/+page.server.js
@@ -9,3 +9,20 @@ export const load = (async () => {
 		majaselections: JSON.stringify(maja_resp)
   }
 })
+
+export const actions = {
+	addnewmaja: async ({ request }) => {
+		const data = await request.formData()
+		const maja_name = data.get('maja_name')?.toString() ?? ''
+
+		const maja_resp = await SelectionModel.find({}).sort({ itemId: 1 }).lean()
+		const total_length = maja_resp.length
+
+		const add_majaresp = await SelectionModel.create({
+			itemId: total_length + 1,
+			text: maja_name
+		})
+
+		return { added: add_majaresp } // add_majadata.inserted.itemId
+	}
+} // satisfies Actions
