d6e039b7138d01422fbf631e7f9635c1c2984db1
Ryo <rchiba8030@gmail.com>
2023-12-01 21:16:49 +0900

    3mm

--- src/routes/admin/selections/test/+page.svelte
+++ src/routes/admin/selections/test/+page.svelte
@@ -5,9 +5,11 @@
 	</script>
 
 <section>
+	{#if data.minselections}
 	{#each data.minselections as row}
 		<li>{row.itemId}</li>
 		<li>{row.parentId.text}</li>
 		<li>{row.text}</li>
 	{/each}
+	{/if}
 </section>
