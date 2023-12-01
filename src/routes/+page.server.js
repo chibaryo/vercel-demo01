b8233c5322728cee2fe460ac1904353c77709df3
Ryo <rchiba8030@gmail.com>
2023-12-01 19:56:29 +0900

    2

---
+++ src/routes/+page.server.js
@@ -0,0 +1,10 @@
+import { API_KEY } from '$env/static/private'
+
+/** @type {import('./$types').PageServerLoad} */
+export const load = (async ({ params }) => {
+
+	return { 
+		message: 'ok',
+		yourkey: API_KEY
+	}
+})
