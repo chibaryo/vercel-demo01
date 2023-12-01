5961bedcf0f5585380b0da0885a680494f33ca1d
Ryo <rchiba8030@gmail.com>
2023-12-01 22:44:08 +0900

    4mm

--- src/routes/admin/selections/test/+page.server.js
+++ src/routes/admin/selections/test/+page.server.js
@@ -2,10 +2,17 @@ import { GrandChildModel, SelectionModel } from '$lib/mongodb/models/Selection'
 import { connectDB } from '$lib/mongodb/plugins/dbconnection'
 
 export const load = (async () => {
-	await connectDB()
-	const resp = await GrandChildModel.find({}).populate('parentId').sort({ "parentId.text": 1 }).lean()
+//	await connectDB()
+//	const resp = await GrandChildModel.find({}).populate('parentId').sort({ "parentId.text": 1 }).lean()
+	const resp = await fetch('/api/selections/min', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	})
+	console.log("resp: ", resp)
 
   return {
-		minselections: JSON.stringify(resp)
+//		minselections: JSON.stringify(resp)
   }
 }) // satisfies PageServerLoad
