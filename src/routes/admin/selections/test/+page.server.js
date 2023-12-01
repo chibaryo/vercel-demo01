37b09cec85aa1d1ea8d2def530584655f6e1b112
Ryo <rchiba8030@gmail.com>
2023-12-01 20:54:37 +0900

    3m

--- src/routes/admin/selections/test/+page.server.js
+++ src/routes/admin/selections/test/+page.server.js
@@ -2,14 +2,15 @@ import { GrandChildModel, SelectionModel } from '$lib/mongodb/models/Selection'
 import { connectDB } from '$lib/mongodb/plugins/dbconnection'
 
 export const load = (async () => {
-	const minresp = await fetch(`/api/selections/min`, {
-		method: 'GET',
-	})
-	const min_retdata = await minresp.json()
+//	const minresp = await fetch(`/api/selections/min`, {
+//		method: 'GET',
+///	})
+//	const min_retdata = await minresp.json()
+	console.log("vercel api server url: ", 'https://' + process.env.VERCEL_URL + '/api/selections/min')
 
-//	await connectDB()
+	//	await connectDB()
 //	const resp = await GrandChildModel.find({}).populate('parentId').sort({ "parentId.text": 1 }).lean()
-	console.log("min_retdata: ", min_retdata)
+//	console.log("min_retdata: ", min_retdata)
 
   return {
 //		minselections: min_retdata["minselections"]
