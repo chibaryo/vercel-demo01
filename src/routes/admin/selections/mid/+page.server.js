6396fe5c12882390c6466bca6f4c08b52f3162ab
Ryo <rchiba8030@gmail.com>
2023-12-03 11:57:48 +0900

    2mm

--- src/routes/admin/selections/mid/+page.server.js
+++ src/routes/admin/selections/mid/+page.server.js
@@ -31,24 +31,42 @@ export const actions = {
 	},
 	addbycsv: async ({ request }) => {
 		const data = await request.formData()
-		const csv = data.get('target')
+		const csv = data.get('sourcefile')
 
 		console.log("csv.type: ", csv.type)
 		let csvArray = new Uint8Array(await csv.arrayBuffer())
 		const str = new TextDecoder().decode(csvArray)
 		const lines = str.split('\r\n')
 		let tmp_arr = []
-		const csv_rows = lines.filter(i => {
+		const obj = Object.assign({}, lines.filter(i => {
+			const csv_row = i.split(',')
+			if (Number(csv_row[0]) >= 1)
+				return true
+		}).map((item) => ({
+			itemId: item[0]
+		})))
+		console.log("obj: ", obj)
+
+		const csv_rows = lines.filter(async (i) => {
 			const csv_row = i.split(',')
 			if (Number(csv_row[0]) >= 1){
-				tmp_arr.push({ itemId: Number(csv_row[0]), parentId: csv_row[1], text: csv_row[2] })
+//				let parentId_id = await SelectionModel.find({ itemId: csv_row[1] }).lean()
+//				console.log("parentId_id", parentId_id[0]["_id"].toString())
+//				tmp_arr.push({ itemId: Number(csv_row[0]), parentId: parentId_id[0]["_id"].toString(), text: csv_row[2] })
 				return true
 			}
 		})
 
-		const addmany_resp = await GrandChildModel.insertMany(tmp_arr)
-		console.log("addmany_resp", addmany_resp)
 
+		await connectDB()
+		csv_rows.shift()
+//		console.log("tmp_arr: ", tmp_arr)
+		console.log("csv_rows: ", csv_rows)
+
+//		const addmany_resp = await ChildModel.insertMany(tmp_arr)
+//		console.log("addmany_resp", addmany_resp)
+
+//		return { added: JSON.stringify(addmany_resp) }
 		return
 	},
 	addminselpost: async ({ request	}) => {
--- src/routes/admin/selections/mid/+page.svelte
+++ src/routes/admin/selections/mid/+page.svelte
@@ -182,10 +182,16 @@
 
 <!-- delete many -->
 <DeleteManyModal visible={deleteManyModalOpen}>
-	<form method="post" action="?/deletebycsv" enctype="multipart/form-data">
+	<form method="post" action="?/deletebycsv" enctype="multipart/form-data" use:enhance={() => {
+		return async ({ result }) => {
+			invalidateAll()
+			await applyAction(result)
+			// Do something
+		}
+	}}>
 		<div style="background-color: rgb(231 229 228); display: flex; flex-flow: column;">
-			<label for="file">File</label>
-			<input type="file" name="target" />
+			<label for="sourcefile">File</label>
+			<input type="file" name="sourcefile" />
 		</div>
 		<div style="background-color: rgb(231 229 228); display: flex; flex-flow: column;">
 			<label for="lowerlimit">範囲(from)</label>
@@ -196,20 +202,26 @@
 			<input type="number" name="upperlimit" />
 		</div>
 		<button type="submit" style="border: 1px; background-color: rgb(255 237 213);">Submit</button>
-		<button on:click={() => closeDeleteMany()} style="background-color: rgb(254 226 226);">Cancel</button>
 	</form>
+	<button on:click={() => closeDeleteMany()} style="background-color: rgb(254 226 226);">Cancel</button>
 </DeleteManyModal>
 
 <!-- insert many -->
 <InsertManyCsvModal visible={insertManyFlag}>
-	<form method="post" action="?/addbycsv" enctype="multipart/form-data">
+	<form method="post" action="?/addbycsv" enctype="multipart/form-data" use:enhance={() => {
+		return async ({ result }) => {
+			invalidateAll()
+			await applyAction(result)
+			// Do something
+		}
+	}}>
 		<div style="background-color: rgb(231 229 228); display: flex; flex-flow: column;">
-			<label for="file">File</label>
-			<input type="file" name="target" />
+			<label for="sourcefile">File</label>
+			<input type="file" name="sourcefile" />
 		</div>
 		<button type="submit" style="border: 1px; background-color: rgb(255 237 213);">Submit</button>
-		<button on:click={() => closeInsertMany()} style="background-color: rgb(254 226 226);">Cancel</button>
 	</form>
+	<button on:click={() => closeInsertMany()} style="background-color: rgb(254 226 226);">Cancel</button>
 </InsertManyCsvModal>
 
 <!-- Add New Major selection modal -->
